76c531ff4eeee96d01ca57f7dac9abe42490d982
jirenguSanyue <562441461@qq.com>
2018-06-28 17:32:49 +0800

    新建leanCloud.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import 'normalize.css'
 import './reset.css'
 import * as localStore from './localStore'
 
-import AV from 'leancloud-storage'
-var APP_ID = 'Ccm488O3B6M5kTnrGiDGqGsu-gzGzoHsz'
-var APP_KEY = 'NJQPLazC78Y8EkcGQ46wnOvj'
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-})
+
 
 
 class App extends Component {
---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'Ccm488O3B6M5kTnrGiDGqGsu-gzGzoHsz'
+var APP_KEY = 'NJQPLazC78Y8EkcGQ46wnOvj'
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
