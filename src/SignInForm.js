e37c3d01408b3e02f213c8fde6c6cf346030326e
jirenguSanyue <562441461@qq.com>
2018-06-29 20:10:18 +0800

    重构之将忘记密码也抽离成一个组件

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class SignInForm extends Component{
     render(){
         return (
-            <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
+            <form className="signIn" onSubmit={this.props.onSubmit}> {/* 登录*/}
                 <div className="row">
                         <label>用户名</label>
                         <input type="text" value={this.props.formData.username}
