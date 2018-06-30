673f93098145e8fa6b18ddd467067d6230da0295
jirenguSanyue <562441461@qq.com>
2018-06-30 10:03:22 +0800

    即使有方法的组件也可以改写成一个函数

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 export default function(props){
     return (
         <form className="signUp" onSubmit={props.onSubmit.bind(this)}> {/* 注册*/}
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './TodoInput.css'
 
-export default class TodoInput extends Component {
-    render(){
-        return <input type="text" 
-        value={this.props.content} 
+
+
+function submit (props,e){
+    if (e.key === 'Enter'){
+        props.onSubmit(e)
+    }
+}
+function changeTitle(props,e){
+    props.onChange(e)
+}
+export default function (props){
+    return <input type="text" value={props.content}
         className="TodoInput"
-        onChange={this.changeTitle.bind(this)} 
-        onKeyPress={this.submit.bind(this)}/>
-    }
-    submit(e){
-        if(e.key === 'Enter'){
-            //console.log('用户点击了回车')
-            this.props.onSubmit(e)
-        }
-    }
-    changeTitle(e){
-        this.props.onChange(e)
-    }
+        onChange={changeTitle.bind(null,props)}
+        onKeyPress={submit.bind(null,props)}/>
 }
